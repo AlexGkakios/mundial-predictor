c937137e5fb0ef6cbab0be72527aa23794a4a2ab
AlexGka <alexgakios@gmail.com>
2026-06-11 14:50:42 +0300

    Add New Json

--- players.py
+++ players.py
@@ -1,22 +1,12 @@
 import json
 
-with open("data/players.json", "r", encoding="utf-8") as f:
-    data = json.load(f)
-
-players = data["stickers"]
-
-
-def is_valid_player(p):
-    return (
-        isinstance(p, dict)
-        and p.get("name") not in ["Emblem", "Team Photo"]
-    )
+with open("players.json", "r", encoding="utf-8") as f:
+    players = json.load(f)
 
 
 def get_players_by_team(team_name):
     return [
         p for p in players
         if isinstance(p, dict)
-        and p.get("team") == team_name
-        and is_valid_player(p)
+        and p.get("teamId") == team_name
     ]
